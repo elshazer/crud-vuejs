47ec6226fcbf8c9b221574b0d25195e451774dc0
byronjoseperez2018@gmail.com <Prez Byron>
2019-01-13 17:27:06 -0600

    todo functional

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="text" v-model="person" @keyup.enter="agregar_persona(person)">
+    
+
+    <div>{{person}}</div>
+    <ul>
+     <li v-for=" persona in lista_personas"> 
+      {{persona.name}}
+      <input type="	text" v-model="form.parent_id[persona.id]"@keyup.enter="actualizar_persona(persona.id,form.parent_id[persona.id])">
+      <button @click="eliminar_persona (persona.id)">eliminar </button>
+
+     </li>
+    </ul> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data : function() {
+    return {
+      LAST : 'last',
+      person: 'example',
+      form: {
+        parent_id:[]
+      },
+      lista_personas: [
+        {name:'josse',id:0},
+        {name:'byron',id:2},
+        {name:'moises',id:3},
+
+      ]
+    }
+  },
+  methods :{
+    obtener_persona : function(params){
+      if (params === this.LAST){
+        return this.lista_personas[this.lista_personas.length-1].id+1
+      }
+       return this.lista_personas = this.lista_personas.filter(function(value, index, array){
+          return value.id !=params
+      })[0]
+    },  
+    agregar_persona: function(params){
+      this.lista_personas.push({id: this.obtener_persona(this.LAST), name: params});
+
+    },
+    eliminar_persona: function(params){
+      this.lista_personas = this.lista_personas.filter(function(value, index, array){
+          return value.id !=params
+      })
+    },
+    actualizar_persona: function(id,params){
+      for (var i = 0; i< this.lista_personas.length; i++){
+        if (this.lista_personas[i].id ==id){
+          this.lista_personas[i].name =  params;
+        }
+      }
+    }
+    
+
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
